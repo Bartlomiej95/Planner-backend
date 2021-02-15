02f788bfdb9e7b79762a2dc19a00f704e4c8445c
Bartlomiej95 <panfilbartlomiej@gmail.com>
2021-02-16 00:14:08 +0100

    Add netlify to CORS

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://planer-app.netlify.app"],
     credentials: true,
 }));
 app.use(cookieParser());
