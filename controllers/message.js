f2c5c5536937372dd8beae4403e2e704ad50e027
Bartlomiej95 <panfilbartlomiej@gmail.com>
2021-07-12 19:51:25 +0200

    Create and add methods in controller for message
    We create two methods for message - showAllMessage for logged in user
    and createNewMessage when user want write new message.

---
+++ controllers/message.js
@@ -0,0 +1,43 @@
+import Message from "../models/message.model";
+
+
+export const showAllMessages = async (req, res) => {
+    try {
+        // chcemy pobrac maile, które jako odbiorcę będą miały maila użytkownika
+        // interesują nas maile konkretnego użytkownika
+        const messages = await Message.find({ recipient: req.body.user_email});
+        
+        res.status(200).json({ messages});
+
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+}
+
+export const createNewMessage = async (req, res) => {
+    const { title, recipient, content, sender  } = req.body;
+    
+    try {
+        if( !title || !recipient || !sender){
+            return res.status(401).json({ message: "Not all required fields have been entered"})
+        }
+
+        if( title.length > 200) {
+            return res.status(401).json({ message: "Title can't be longer than 200 characters"})
+        }
+        
+        const newMessage = new Message({
+            title, 
+            recipient,
+            content,
+            sender,
+        });
+
+        const savedMessage = await newMessage.save();
+        console.log(savedMessage);
+
+        res.status(201).json(savedMessage);
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+}
--- routes/homepage/message/message.js
+++ routes/homepage/message/message.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import { createNewMessage, showAllMessages } from '../../../controllers/message.js';
 import auth from '../../../middlewares/auth.js';
 
 const router = express.Router();
 
-
+router.get('/', auth, showAllMessages);
+router.post('/create', auth, createNewMessage);
 
 export default router;
